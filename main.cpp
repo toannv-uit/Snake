05a2da8fa98950952a4daf2cf5cd060c86c01cdc
UIT-SV2 <18521488@gm.uit.edu.vn>
2022-11-16 19:16:49 +0700

    Code phần vẽ khung và gọi ở hàm main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
 using namespace std;
 void gotoxy( int column, int line );
 struct Point{
@@ -33,7 +37,14 @@ public:
 
     }
 };
-
+void VeKhung(){
+    for (int i = MINX ; i<=MAXX ; i++)
+        for (int j = MINX ; j<=MAXY ; j++)
+            if ((i==MINX) || (i==MAXX) || (j==MINY) || (j==MAXY)){
+            gotoxy(i,j);
+            printf("+");
+        }
+}
 int main()
 {
     CONRAN r;
@@ -49,6 +60,7 @@ int main()
             if (t=='x') Huong = 1;
         }
         system("cls");
+        VeKhung();
         r.Ve();
         r.DiChuyen(Huong);
         Sleep(300);

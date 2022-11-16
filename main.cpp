0a76932f49e2adf63e2ea256b18334980369e4e5
UIT-SV3 <toannv@gm.uit.edu.vn>
2022-11-16 19:32:17 +0700

    Ăn mồi

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <time.h>
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
 using namespace std;
 void gotoxy( int column, int line );
 struct Point{
@@ -17,20 +22,25 @@ public:
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
     }
-    void Ve(){
+    void Ve(Point Qua){
         for (int i = 0; i < DoDai; i++){
             gotoxy(A[i].x,A[i].y);
             cout<<"X";
         }
+        gotoxy(Qua.x, Qua.y); cout<<"*";
     }
-    void DiChuyen(int Huong){
+    void DiChuyen(int Huong, Point& Qua){
         for (int i = DoDai-1; i>0;i--)
             A[i] = A[i-1];
         if (Huong==0) A[0].x = A[0].x + 1;
         if (Huong==1) A[0].y = A[0].y + 1;
         if (Huong==2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
-
+        if ((A[0].x == Qua.x) && (A[0].y == Qua.y)){
+            DoDai++;
+            Qua.x = rand()%(MAXX-MINX)+MINX;
+            Qua.y = rand()%(MAXY-MINY)+MINY;
+        }
     }
 };
 
@@ -39,7 +49,10 @@ int main()
     CONRAN r;
     int Huong = 0;
     char t;
-
+    Point Qua;
+    srand((int)time(0));
+    Qua.x = rand()%(MAXX-MINX)+MINX;
+    Qua.y = rand()%(MAXY-MINY)+MINY;
     while (1){
         if (kbhit()){
             t = getch();
@@ -49,8 +62,8 @@ int main()
             if (t=='x') Huong = 1;
         }
         system("cls");
-        r.Ve();
-        r.DiChuyen(Huong);
+        r.Ve(Qua);
+        r.DiChuyen(Huong, Qua);
         Sleep(300);
     }
 
